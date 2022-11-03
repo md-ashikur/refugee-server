ded7e6a0d2cf85194bf4f9ac8cf9d77edfa5b552
unknown <md-ashikur>
2022-11-03 20:34:54 +0600

    All Accomodations load

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ukfxhcm.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log("db connect")
-  // perform actions on the collection object
-  client.close();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
+    const accomodationCollection = client.db("refugeeAccomodation").collection("accomodation");
+
+    // All Accomodations======================
+    app.get("/accomodations", async (req, res) => {
+      const query = {};
+      const cursor = accomodationCollection.find(query);
+      const accomodations = await cursor.toArray();
+      res.send(accomodations);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/home", (req, res) => {
   res.send(accomo);
 });
 
 // add accomodation post
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
