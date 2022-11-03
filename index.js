2c0a98bb07c36fae5f0bfc39a35da2fa36871ea3
unknown <md-ashikur>
2022-11-03 19:43:34 +0600

    mongoDB connect

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello backen World!')
-})
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ukfxhcm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log("db connect")
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get("/home", (req, res) => {
+  res.send(accomo);
+});
+
+// add accomodation post
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
