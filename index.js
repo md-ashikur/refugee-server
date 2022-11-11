14379ba5e3545eed74bc2f25fd616f04eb46df41
unknown <md-ashikur>
2022-11-12 02:38:26 +0600

    changed all api

--- index.js
+++ index.js
@@ -1,135 +1,52 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const ImageModal = require("./image.modal");
-const fs = require("fs");
 const cors = require("cors");
+const multer = require("multer");
 const mongoose = require("mongoose");
-require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const multer = require("multer");
+// connect to mongoDB start====================
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("conneted"))
+  .catch((err) => console.log(err));
+// connect to mongoDB end====================
 
-//storage====================
+// --------------- upload files start--------------------------------
+//storage===
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "Images");
   },
   filename: (req, file, cb) => {
     console.log(file);
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, req.body.name);
   },
 });
 
 const upload = multer({ storage: storage });
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ukfxhcm.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("Image has been uploaded");
 });
+// --------------- upload files end--------------------------------
 
-async function run() {
-  try {
-    await client.connect();
-    const accomodationCollection = client
-      .db("refugeeAccomodation")
-      .collection("accomodation");
-    const userCollection = client
-      .db("refugeeAccomodation")
-      .collection("user");
-
-    // Get Accomodations======================
-    app.get("/accomodations", async (req, res) => {
-      const query = {};
-      const cursor = accomodationCollection.find(query);
-      const accomodations = await cursor.toArray();
-      res.send(accomodations);
-    });
-
-    // POST accomodation ==============
-    app.post("/accomodations", upload.single("image"), async (req, res) => {
-      const newAccomodation = req.body;
-      const result = await accomodationCollection.insertOne(newAccomodation);
-      res.send(result);
-    });
-
-    app.post("/accomodations", upload.single("image"), async (req, res) => {
-      upload(req, res, (err) => {
-        const data = req.body;
-        if (err) {
-          console.log(err);
-        } else {
-          const newImage = new ImageModal({
-            people: data.people,
-            rooms: data.rooms,
-            city: data.city,
-            from: data.from,
-            to: data.to,
-            email: data.email,
-            phone: data.phone,
-            title: data.title,
-            description: data.description,
-            image: {
-              data: fs.readFileSync(
-                path.join(__dirname + "/uploads/" + req.file.filename)
-              ),
-            },
-          });
-          newImage
-            .save()
-            .then(() => res.send("success"))
-            .catch((err) => console.log(err));
-        }
-      });
-    });
-    // Delete accomodation =============
-    app.delete("/accomodations/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const result = await accomodationCollection.deleteOne(query);
-      res.send(result);
-    });
-    // Edit accomodation ================
-    app.put("/accomodations/:id", async (req, res) => {
-      const id = req.params.id;
-      const data = req.body;
-      const filter = { _id: ObjectId(id) };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: {
-          image: data.image,
-          people: data.people,
-          rooms: data.rooms,
-          city: data.city,
-          from: data.from,
-          to: data.to,
-          email: data.email,
-          phone: data.phone,
-          title: data.title,
-          description: data.description,
-        },
-      };
-
-      const result = await accomodationCollection.updateOne(
-        filter,
-        updateDoc,
-        options
-      );
-      res.send(result);
-    });
-  } finally {
-  }
-}
-
-run().catch(console.dir);
-
+// routes start======================
+app.use("/auth", authRoute);
+app.use("/users", userRoute);
+app.use("/posts", postRoute);
+// routes end==============================
 app.get("/home", (req, res) => {
   res.send(accomo);
 });
---
+++ models/Post.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    numberOfPeople: {
+      type: Number,
+      required: true,
+    },
+    numberOfRooms: {
+      type: Number,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    from: {
+      type: String,
+      required: true,
+    },
+    to: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
